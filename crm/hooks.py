819588177d633f0cb0f06250fdd40ea283351394
AnkitChandolaGreenfortune <ankitchandola@thegreenfortune.com>
2026-05-06 00:27:11 +0530

    feat: Add custom_contact_role field to Custom Field and update filters for CRM Lead and Contact in hooks

--- crm/hooks.py
+++ crm/hooks.py
@@ -187,7 +187,7 @@ fixtures = [
 	"CRM Lead Status",
 	{"dt": "Role", "filters": [["is_custom", "=", 1]]},
 	{"dt": "Role Profile", "filters": [["name", "in", ["B2F Team", "Calling Team", "Jr. Sales Executive", "Sales Coordinator", "Sales Executive", "ASM", "Project Sales Executive", "RSM", "Marketing", "Management", "Sales Head", "Estimation Team"]]]},
-	{"dt": "Custom Field", "filters": [["dt", "=", "CRM Lead"]]},
+	{"dt": "Custom Field", "filters": [["dt", "in", ["CRM Lead", "Contact"]]]},
 	{"dt": "Property Setter", "filters": [["doc_type", "=", "CRM Lead"]]},
 	{"dt": "Server Script", "filters": [["module", "=", "FCRM"]]},
 	{"dt": "Assignment Rule", "filters": [["document_type", "=", "CRM Lead"]]},
